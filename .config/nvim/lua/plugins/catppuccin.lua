0feea7b14dc8f4b60092041dc7943c0a542733f3
fndmossmann <1223711+fndmossmann@users.noreply.github.com>
2025-04-08 11:05:30 -0300

    neovim: update lualine and catppuccin configs

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,8 @@
 return {
-	"catppuccin/nvim", 
-	name = "catppuccin", 
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
-	
+
 	opts = {
 		flavour = "mocha"
 	}
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,8 +3,18 @@ return {
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
 		options = {
-			component_separators = { left = '', right = ''},
-			section_separators = { left = '', right = ''},
+			-- theme =
+			section_separators = { left = '', right = '' },
+			component_separators = { left = '', right = '' }
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = { "" },
+			lualine_y = { "%l:%c", "encoding", "filetype", "lsp_status" },
+			lualine_z = { "os.date('%H:%M')", "hostname" },
 		}
 	}
 }
+
