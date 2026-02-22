a2b47801a9fa862977bf3030a7b3c7fc545a72ce
fndmossmann <1223711+fndmossmann@users.noreply.github.com>
2026-02-22 17:57:06 -0300

    bashrc: update SSH_AUTH_SOCK env var

--- .bashrc.d/04-environment.sh
+++ .bashrc.d/04-environment.sh
@@ -1,5 +1,5 @@
 
 
 if [ "$HOSTNAME" == eperm ] ; then
-	export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+	export SSH_AUTH_SOCK="/home/$USER/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
 fi
